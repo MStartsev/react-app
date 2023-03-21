16943447553c18d92edc11d0dfc224d2914a4b42
M. A. Startsev <m.a.startsev@gmail.com>
2023-03-21 04:41:21 +0200

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
+let a = "1";
 
-const post = (
-  <>
-    <h2>Post Header</h2>
-    <p>Post text</p>
-  </>
-);
-
-const favouriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" },
-  { id: "id-4", name: "Redux in depth" },
-];
-
-const BookList = ({ books }) => {
-  return (
-    <ul>
-      {books.map((book) => (
-        <li>{book.name}</li>
-      ))}
-    </ul>
-  );
-};
+// a = 11;
 
 function App() {
   document.title = "ᐉ ZRN • ВТК";
@@ -32,26 +11,32 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-
-      {post}
-
-      <div>
-        <BookList books={favouriteBooks} />
-      </div>
+      {a && <h2>Hello</h2>}
+      <img
+        src="https://drive.google.com/uc?export=view&id=1-O9jGiRvEFrKT9PYL7TrRMZEvyI-udv9"
+        className="img"
+        alt="logo"
+      />
     </div>
   );
 }
 
 export default App;
+// "https://drive.google.com/file/d/1-O9jGiRvEFrKT9PYL7TrRMZEvyI-udv9/view?usp=share_link"
+// const favouriteBooks = [
+//   { id: "id-1", name: "JS for beginners" },
+//   { id: "id-2", name: "React basics" },
+//   { id: "id-3", name: "React Router overview" },
+//   { id: "id-4", name: "Redux in depth" },
+// ];
+
+// const BookList = ({ books }) => {
+//   return (
+//     <ul>
+//       {books.map((book) => (
+//         <li>{book.name}</li>
+//       ))}
+//     </ul>
+//   );
+// };
