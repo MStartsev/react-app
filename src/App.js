60e9e64bc285212c3467aa35390cfae0223764e2
M. A. Startsev <m.a.startsev@gmail.com>
2023-03-22 08:02:39 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,38 @@ let a = "1";
 
 // a = 11;
 
+const getData = async () => {
+  const url =
+    "https://script.google.com/macros/s/AKfycbzncnMcwpLCjoPs-tluBEh6s1E8o8LP3aqAvemrJOkUp23D_JY016AcjKWxsf5MpoDoxg/exec";
+
+  // fetch(url)
+  //   .then((d) => d.json())
+  //   .then((d) => {
+  //     document.getElementById("app").textContent = d[0].name;
+  //     console.log(d);
+  //   });
+
+  let data = "Olya";
+
+  const params = {
+    // redirect: "follow",
+    method: "POST",
+    contentType: "application/jsonp",
+    payload: JSON.stringify(data),
+    // mode: "no-cors",
+    // cache: "no-cache",
+    // credential: "same-origin",
+    // headers: {"Content-Type": "application/json",},
+    // direct: "fillow",
+    // reffererPolicy: "no-refferer",
+    // body: JSON.stringify({ spec: base }),
+  };
+
+  const res = await fetch(url, params);
+  const json = await res.json(); // додатково оброблюємо Promise
+  console.log(json);
+};
+
 function App() {
   document.title = "ᐉ ZRN • ВТК";
 
@@ -13,11 +45,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       {a && <h2>Hello</h2>}
+      <button onClick={(event) => getData()}>Click me!</button>
       <img
         src="https://drive.google.com/uc?export=view&id=1-O9jGiRvEFrKT9PYL7TrRMZEvyI-udv9"
         className="img"
         alt="logo"
       />
+
+      <p id="app">0</p>
     </div>
   );
 }
