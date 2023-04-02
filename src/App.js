d68eef284eb46f3da7647c7efdb87209b64eac9c
M. A. Startsev <m.a.startsev@gmail.com>
2023-04-02 19:26:28 +0300

    #

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
-let a = "1";
+import React from 'react';
+import logo from './logo.svg';
+
+let a = '1';
 
 // a = 11;
 
 const getData = async () => {
   const url =
-    "https://script.google.com/macros/s/AKfycbzncnMcwpLCjoPs-tluBEh6s1E8o8LP3aqAvemrJOkUp23D_JY016AcjKWxsf5MpoDoxg/exec";
+    'https://script.google.com/macros/s/AKfycbzncnMcwpLCjoPs-tluBEh6s1E8o8LP3aqAvemrJOkUp23D_JY016AcjKWxsf5MpoDoxg/exec';
 
   // fetch(url)
   //   .then((d) => d.json())
@@ -15,12 +16,12 @@ const getData = async () => {
   //     console.log(d);
   //   });
 
-  let data = "Olya";
+  let data = 'Olya';
 
   const params = {
     // redirect: "follow",
-    method: "POST",
-    contentType: "application/jsonp",
+    method: 'POST',
+    contentType: 'application/jsonp',
     payload: JSON.stringify(data),
     // mode: "no-cors",
     // cache: "no-cache",
@@ -37,7 +38,7 @@ const getData = async () => {
 };
 
 function App() {
-  document.title = "ᐉ ZRN • ВТК";
+  document.title = 'ᐉ ZRN • ВТК';
 
   return (
     <div className="App">
@@ -45,7 +46,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       {a && <h2>Hello</h2>}
-      <button onClick={(event) => getData()}>Click me!</button>
+      <button onClick={event => getData()}>Click me!</button>
       <img
         src="https://drive.google.com/uc?export=view&id=1-O9jGiRvEFrKT9PYL7TrRMZEvyI-udv9"
         className="img"
@@ -58,20 +59,3 @@ function App() {
 }
 
 export default App;
-// "https://drive.google.com/file/d/1-O9jGiRvEFrKT9PYL7TrRMZEvyI-udv9/view?usp=share_link"
-// const favouriteBooks = [
-//   { id: "id-1", name: "JS for beginners" },
-//   { id: "id-2", name: "React basics" },
-//   { id: "id-3", name: "React Router overview" },
-//   { id: "id-4", name: "Redux in depth" },
-// ];
-
-// const BookList = ({ books }) => {
-//   return (
-//     <ul>
-//       {books.map((book) => (
-//         <li>{book.name}</li>
-//       ))}
-//     </ul>
-//   );
-// };
