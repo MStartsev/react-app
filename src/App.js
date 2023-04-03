4cdecccef9301e283c1b71f9af7501b50b27147d
M. A. Startsev <m.a.startsev@gmail.com>
2023-04-03 07:18:35 +0300

    add get

--- src/App.js
+++ src/App.js
@@ -1,44 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./zrn.svg";
+import axios from "axios";
+import { CorsOptions } from "cors";
 
-let a = '1';
+let a = "1";
 
 // a = 11;
 
-const getData = async () => {
-  const url =
-    'https://script.google.com/macros/s/AKfycbzncnMcwpLCjoPs-tluBEh6s1E8o8LP3aqAvemrJOkUp23D_JY016AcjKWxsf5MpoDoxg/exec';
+const BASE_URL =
+  "https://script.google.com/macros/s/AKfycbwYEge-cAgbX8cR2IncbpGVB-_T9S4VzCJyU_iT6Tud/dev";
 
-  // fetch(url)
-  //   .then((d) => d.json())
-  //   .then((d) => {
-  //     document.getElementById("app").textContent = d[0].name;
-  //     console.log(d);
-  //   });
-
-  let data = 'Olya';
-
-  const params = {
-    // redirect: "follow",
-    method: 'POST',
-    contentType: 'application/jsonp',
-    payload: JSON.stringify(data),
-    // mode: "no-cors",
-    // cache: "no-cache",
-    // credential: "same-origin",
-    // headers: {"Content-Type": "application/json",},
-    // direct: "fillow",
-    // reffererPolicy: "no-refferer",
-    // body: JSON.stringify({ spec: base }),
+export const fetchPost = async (query = "", page = 1) => {
+  const searchParams = {
+    method: "POST",
+    CorsOptions,
+    body: JSON.stringify({
+      q: query,
+      orientation: "horizontal",
+      safesearch: true,
+      page: page,
+    }),
   };
 
-  const res = await fetch(url, params);
-  const json = await res.json(); // додатково оброблюємо Promise
-  console.log(json);
+  try {
+    const response = await axios.post(BASE_URL, searchParams);
+
+    return response.json();
+  } catch (e) {
+    console.log("qqq");
+    return e;
+  }
+};
+
+const dataPost = async () => {
+  fetch(BASE_URL, {
+    redirect: "follow",
+    method: "POST",
+    body: JSON.stringify([{ a: 1 }, { b: 2 }]),
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+const getData = async () => {
+  const url =
+    " https://script.google.com/macros/s/AKfycbxIeRVEiiIlqOSaiX1oe3CtN6-x-Ra29Gt30KRkBTcIaIzYavkYUx2wTbHF1EQL6nuTTw/exec?start=1325437200&end=1325439000&prefix=alert";
+
+  const res = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(window.history);
+    });
 };
 
 function App() {
-  document.title = 'ᐉ ZRN • ВТК';
+  document.title = "ᐉ ZRN • ВТК";
 
   return (
     <div className="App">
@@ -46,14 +69,20 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       {a && <h2>Hello</h2>}
-      <button onClick={event => getData()}>Click me!</button>
+      <a href="https://script.google.com/macros/s/AKfycbwctKHM50LYWnZwYkUuaySyKcc62WQOMmaoyHemn9CxQdB_8bKjuldZ4Ie2OyCbSCO1UQ/exec?start=1325437200&end=1325439000&prefix=alert">
+        "ttt eqweqw qwe qw qweq we qwe qwe qweqweq we qwe q"
+      </a>
+      <button onClick={(event) => getData()}>Click me!</button>
       <img
         src="https://drive.google.com/uc?export=view&id=1-O9jGiRvEFrKT9PYL7TrRMZEvyI-udv9"
         className="img"
         alt="logo"
+        width="400px"
+        height="400px"
       />
 
       <p id="app">0</p>
+      <script src="https://script.google.com/macros/s/AKfycbwctKHM50LYWnZwYkUuaySyKcc62WQOMmaoyHemn9CxQdB_8bKjuldZ4Ie2OyCbSCO1UQ/exec?start=1325437200&end=1325439000&prefix=alert"></script>
     </div>
   );
 }
