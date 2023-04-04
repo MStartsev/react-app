bc25ba90f362e621c59ab4350d8cdca8e14c85fa
M. A. Startsev <m.a.startsev@gmail.com>
2023-04-04 06:43:47 +0300

    add post

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
 import React from "react";
 import logo from "./zrn.svg";
-import axios from "axios";
-import { CorsOptions } from "cors";
 
 let a = "1";
 
 // a = 11;
 
 const BASE_URL =
-  "https://script.google.com/macros/s/AKfycbwYEge-cAgbX8cR2IncbpGVB-_T9S4VzCJyU_iT6Tud/dev";
+  "https://script.google.com/macros/s/AKfycbwuH3wG19viE1klcnPfZro60F7Mgtz2hE8FJsFGNfgh3Q0aPp7pUBtleS0H_wTGZSoH5A/exec";
 
-export const fetchPost = async (query = "", page = 1) => {
-  const searchParams = {
+export async function postData() {
+  let res = { name: "Bobby" };
+
+  console.info(res);
+
+  await fetch(BASE_URL, {
     method: "POST",
-    CorsOptions,
-    body: JSON.stringify({
-      q: query,
-      orientation: "horizontal",
-      safesearch: true,
-      page: page,
-    }),
-  };
-
-  try {
-    const response = await axios.post(BASE_URL, searchParams);
-
-    return response.json();
-  } catch (e) {
-    console.log("qqq");
-    return e;
-  }
-};
-
-const dataPost = async () => {
-  fetch(BASE_URL, {
-    redirect: "follow",
-    method: "POST",
-    body: JSON.stringify([{ a: 1 }, { b: 2 }]),
+    mode: "no-cors",
+    cache: "no-cache",
+    // credential: "same-origin",
     headers: {
-      "Content-Type": "text/plain;charset=utf-8",
-      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
     },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+    direct: "fillow",
+    reffererPolicy: "no-refferer",
+    body: JSON.stringify(res),
+  });
+}
+
+const fetchData = async (scriptURL, data) => {
+  var json;
+  try {
+    const response = await fetch(scriptURL, {
+      method: "POST",
+      body: JSON.stringify(data),
     });
+    if (response !== "") json = await response.json();
+    console.log("Success:", JSON.stringify(json));
+  } catch (e) {
+    console.log("Errors:", e.message);
+  }
+  return json;
 };
+
+async function main() {
+  const res = await fetchData(BASE_URL, {
+    name: "Bobby",
+  });
+  console.log(res);
+}
 
 const getData = async () => {
   const url =
-    " https://script.google.com/macros/s/AKfycbxIeRVEiiIlqOSaiX1oe3CtN6-x-Ra29Gt30KRkBTcIaIzYavkYUx2wTbHF1EQL6nuTTw/exec?start=1325437200&end=1325439000&prefix=alert";
+    "https://script.google.com/macros/s/AKfycbwuH3wG19viE1klcnPfZro60F7Mgtz2hE8FJsFGNfgh3Q0aPp7pUBtleS0H_wTGZSoH5A/exec?start=1325437200&end=1325439000&prefix=alert";
 
   const res = await fetch(url)
     .then((response) => response.json())
@@ -72,7 +73,8 @@ function App() {
       <a href="https://script.google.com/macros/s/AKfycbwctKHM50LYWnZwYkUuaySyKcc62WQOMmaoyHemn9CxQdB_8bKjuldZ4Ie2OyCbSCO1UQ/exec?start=1325437200&end=1325439000&prefix=alert">
         "ttt eqweqw qwe qw qweq we qwe qwe qweqweq we qwe q"
       </a>
-      <button onClick={(event) => getData()}>Click me!</button>
+      <button onClick={(event) => getData()}>Click me! GET</button>
+      <button onClick={(event) => main()}>Click me! POST</button>
       <img
         src="https://drive.google.com/uc?export=view&id=1-O9jGiRvEFrKT9PYL7TrRMZEvyI-udv9"
         className="img"
